adf04834d182f72b415f8fee75152e36d914807f
0114koki <koki.kurihara.0114@gmail.com>
2023-05-30 14:02:45 +0900

    commit

--- run.py
+++ run.py
@@ -6,6 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello World!"
+
+@app.route('/flask')
+def hello_sample():
+    return "Hello Flask."
+
+@app.route('/user/<user_id>')
+def hello_person(user_id):
+    return "Hello " + user_id
+
+@app.route('/test', methods=['GET', 'POST'])
+def post_test():
+    if request.method == 'GET':
+        pass
+    else:
+        pass
+    return '0'
+
 #最後に上記の通り定義したアプリケーションを起動しています。
 if __name__ == '__main__':
     app.run()
